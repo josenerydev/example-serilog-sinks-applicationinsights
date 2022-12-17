1c2114fe16e53ed34d37b7bb5850964fd66356d1
José Nery <josenerydev@gmail.com>
2022-12-17 00:06:17 -0300

    log starting web and exception

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # example-serilog-sinks-applicationinsights
+1 - https://github.com/serilog/serilog-aspnetcore
+2 - https://github.com/serilog-contrib/serilog-sinks-applicationinsights
+3 - https://github.com/serilog/serilog-sinks-async
--- example-serilog-sinks-applicationinsights/Program.cs
+++ example-serilog-sinks-applicationinsights/Program.cs
@@ -1,26 +1,72 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.ApplicationInsights.Extensibility;
+using Serilog;
+using System.Reflection;
 
-// Add services to the container.
+var configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json")
+        .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", true)
+        .AddUserSecrets(Assembly.GetExecutingAssembly(), false)
+        .AddEnvironmentVariables()
+        .Build();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]);
+var guid = Guid.NewGuid().ToString().Substring(0, 4);
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+var telemetryConfiguration = new TelemetryConfiguration
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    ConnectionString = configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]
+};
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .WriteTo.ApplicationInsights(telemetryConfiguration, TelemetryConverter.Traces)
+    .CreateBootstrapLogger();
+
+try
+{
+    Log.Information("Starting web application {0}", guid);
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((context, services, loggerConfiguration) => loggerConfiguration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .WriteTo.ApplicationInsights(telemetryConfiguration, TelemetryConverter.Traces));
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddApplicationInsightsTelemetry();
+
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    //throw new Exception($"Exception for test {guid}");
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly {0}", guid);
+}
+finally
+{
+    Log.CloseAndFlush();
+}
